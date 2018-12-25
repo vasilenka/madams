1cce3ee85b48c605701f2d1f88e61ee915dd83bc
vasilenka <bhmantan@gmail.com>
2018-12-25 13:49:59 +0700

    update homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, './../public', 'index.html'));
 });
 
 module.exports = router;
